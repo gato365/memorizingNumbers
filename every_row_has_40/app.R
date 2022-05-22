ddb0a042d4996cbc5ec782d920d88864939aa634
Immanuel Williams <immanuelwilliams@gato365.com>
2022-05-22 08:18:02 -0700

    almost finish user input

--- every_row_has_40/app.R
+++ every_row_has_40/app.R
@@ -10,41 +10,59 @@ setwd("C:/Users/james/OneDrive/Documents/Important_Files/Life/01_thoughts_self/0
 source('bring_in_e.R')
 
 
+select_set_fun <- function(specified_set) {
+  ## The Solution
+  value_to_test = e_number_df %>% 
+    filter(set == specified_set) %>% 
+    select(-set) %>% 
+    unite('Merged', `col-1`:`col-4`,remove =FALSE,sep = '') %>% 
+    mutate(Merged = str_remove_all(Merged,'\'')) %>% 
+    pull(Merged)
+  return(value_to_test)
+}
+
+
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("How many Numbers are known of e are memorized?"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      
+      
+      selectInput('which_set','Select Set to Test',choices = e_number_df$set),
+ 
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      ## Checking 1 set
+      textInput("solution_number", label = h2("Set Numbers"), value = "",width = "400px"),
+      verbatimTextOutput("evaluation"),
+      radioButtons('answer','Show Answer', c('Nothing','Answer','Location'), selected = 'Nothing'),
+      htmlOutput("eval_answer")
+      
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
 }
 
 # Run the application 

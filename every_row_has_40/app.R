fe5f76e0bba239763e2b8e849cd3cdd0a6db91c6
Immanuel Williams <immanuelwilliams@gato365.com>
2022-05-23 05:36:00 -0700

    breakdown of the code

--- every_row_has_40/app.R
+++ every_row_has_40/app.R
@@ -38,16 +38,16 @@ ui <- fluidPage(
       
       
       selectInput('which_set','Select Set to Test',choices = e_number_df$set),
- 
+      
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
       ## Checking 1 set
       textInput("solution_number", label = h2("Set Numbers"), value = "",width = "400px"),
-      verbatimTextOutput("evaluation"),
-      radioButtons('answer','Show Answer', c('Nothing','Answer','Location'), selected = 'Nothing'),
-      htmlOutput("eval_answer")
+      # verbatimTextOutput("evaluation"),
+      # radioButtons('answer','Show Answer', c('Nothing','Answer','Location'), selected = 'Nothing'),
+      # htmlOutput("eval_answer")
       
     )
   )
@@ -64,25 +64,17 @@ server <- function(input, output) {
   ##
   ##----------------------------
   output$evaluation <- renderPrint({ 
-    # ## Get My answer
-    # emans_solution = str_extract_all(input$solution_number, boundary("character"))[[1]]
-    # ## The Solution
-    # reals_solution = str_extract_all(select_set_fun(input$which_set), boundary("character"))[[1]] %>% 
-    #   str_remove('\\.') %>% 
-    #   str_subset( ".+")
-    # 
-    # numbers_tested = length(reals_solution)
-    # ## Check
-    # paste0(round(sum(emans_solution == reals_solution)/ numbers_tested,3) * 100, ' %')
-
-  
-    number_row = input$which_set
+    
+    info = input$which_set
+    number_row = as.numeric(str_remove(info,'set-'))
+    
     ## Get My answer
     emans_solution = str_extract_all(input$solution_number, boundary("character"))[[1]]
     
     
+    
     tmp_df = df %>% 
-      filter(labeled == input$set_combination)
+      slice(number_row)
     
     ## The Solution
     reals_solution = str_extract_all(pull(tmp_df[number_row,1],Merged), boundary("character"))[[1]] %>% 
@@ -116,32 +108,34 @@ server <- function(input, output) {
     # }
     
     
+    # info = input$which_set
+    # number_row = as.numeric(str_remove(info,'set-'))
+    # tmp_df = df %>% 
+    #   # filter(labeled == input$set_combination)
+    #   slice(number_row)
+    # 
+    # ## Get My answer
+    # emans_solution = str_extract_all(input$solution_number, boundary("character"))[[1]]
+    # 
+    # 
+    # ## The Solution
+    # reals_solution = str_extract_all(pull(tmp_df[info,1],Merged), boundary("character"))[[1]] %>% 
+    #   str_remove('\\.') %>% 
+    #   str_subset( ".+")
+    # 
+    # ## User wants to see answer
+    # if(input$answer1 == 'Answer'){
+    #   HTML(paste(reals_solution,collapse = '')) 
+    # } else if(input$answer1 == 'Location') {
+    #   HTML(paste(ifelse(emans_solution == reals_solution,1,0),collapse = ''))
+    # } else if(input$answer1 == 'Nothing'){
+    #   HTML('')
+    # }
     
-    number_row = input$which_set
-    tmp_df = df %>% 
-      filter(labeled == input$set_combination)
-    
-    ## Get My answer
-    emans_solution = str_extract_all(input$solution_number, boundary("character"))[[1]]
-    
-    
-    ## The Solution
-    reals_solution = str_extract_all(pull(tmp_df[number_row,1],Merged), boundary("character"))[[1]] %>% 
-      str_remove('\\.') %>% 
-      str_subset( ".+")
-    
-    ## User wants to see answer
-    if(input$answer1 == 'Answer'){
-      HTML(paste(reals_solution,collapse = '')) 
-    } else if(input$answer1 == 'Location') {
-      HTML(paste(ifelse(emans_solution == reals_solution,1,0),collapse = ''))
-    } else if(input$answer1 == 'Nothing'){
-      HTML('')
-    }
-    
+    cat('s')
     
   })
- 
+  
 }
 
 # Run the application 

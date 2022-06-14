215d97ff60b0100f50a6893a3e8eedb0890a667d
Immanuel Williams <immanuelwilliams@gato365.com>
2022-06-13 22:43:13 -0700

    working model

--- testing_textbox/app.R
+++ testing_textbox/app.R
@@ -117,9 +117,9 @@ server <- function(input, output) {
     info_given = str_split(input$input_text,'\n')[[1]]
     
     
+    info = ''
     
-    
-    if(sum(str_detect(input$input_text,'\n')) >= 0){            ## 1st Line
+    if(sum(str_detect(input$input_text,'\n')) == 0){            ## 1st Line
       emansSolutionLine = info_given[1]
       grading1 = displayGrade(1,emansSolutionLine,tmp_df)
       info = grading1
@@ -129,25 +129,24 @@ server <- function(input, output) {
       emansSolutionLine = info_given[2]
       grading2 = displayGrade(2,emansSolutionLine,tmp_df)
       info = paste0(info, tags$br(), grading2)
-    }
+    } 
     
     if(sum(str_detect(input$input_text,'\n')) == 2){    ## 3rd Line
       emansSolutionLine = info_given[3]
       grading3 = displayGrade(3,emansSolutionLine,tmp_df)
-      info = paste0(info, '\n','Line 3: \n', grading3)
+      info = paste0(info, tags$br(), grading3)
     }
     
     if(sum(str_detect(input$input_text,'\n')) == 3){     ## 4th Line
       emansSolutionLine = info_given[4]
       grading4 = displayGrade(4,emansSolutionLine,tmp_df)
-      
-      info = paste0(info, '\n','Line 4: \n', grading4)
+      info = paste0(info, tags$br(), grading4)
     }
     
     if(sum(str_detect(input$input_text,'\n')) == 4){     ## 5th Line
       emansSolutionLine = info_given[5]
       grading5 = displayGrade(5,emansSolutionLine,tmp_df)
-      info = paste0(info, '\n','Line 5: \n', grading5)
+      info = paste0(info, tags$br(), grading5)
     }
     
     
